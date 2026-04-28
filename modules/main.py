7657fcf1639cc8afa8df21bee9132c3cd22e8539
creator154 <cteator898@gmail.com>
2026-04-28 13:40:56 +0530

    Update status checking message in main.py

--- modules/main.py
+++ modules/main.py
@@ -148,7 +148,8 @@ async def start(client: Client, msg: Message):
     await asyncio.sleep(1)
     await start_message.edit_text(
         Data.START.format(msg.from_user.mention) +
-        "ᴄʜᴇᴄᴋɪɴɢ ꜱᴛᴀᴛᴜꜱ ᴀᴄᴛɪᴠᴇ... Command Pta Hai Ki Nhi Sirji 🙃 Contact @Itz_Sumit**🔍\n\n"
+        "ᴄʜᴇᴄᴋɪɴɢ ꜱᴛᴀᴛᴜꜱ ᴀᴄᴛɪᴠᴇ... ᴄᴏᴍᴍᴀɴᴅ ᴘᴛᴀ ʜᴀɪ ᴋɪ ɴʜɪ ꜱɪʀᴊɪ 🙃  
+ᴄᴏɴᴛᴀᴄᴛ @Itz_Sumit**🔍\n\n"
         "ᴘʀᴏɢʀᴇꜱꜱ:[🟩🟥🟩🟥🟩🟥🟩🟥🟩] 100%\n\n"
     )
 
