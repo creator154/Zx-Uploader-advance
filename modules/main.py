105e6b1c4c7856e9e055e822b3d8af91d2beb762
creator154 <cteator898@gmail.com>
2026-05-16 16:53:31 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -31,7 +31,7 @@ bot = Client(
     "bot",
     api_id=11867213,
     api_hash="d475e13d8cf6937316d9fb8df5a049f9",
-    bot_token="8367993215:AAH_LJAfSbiQnLcnQguV12DrfCl_cfCyMMw"
+    bot_token="8753481555:AAFOijGVEk5huZnARmIUDKVzmUITifo8SNc"
 )
 
 my_name = "ZX"
