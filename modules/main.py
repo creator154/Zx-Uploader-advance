6043b7fbac5c86a03bff8f793c0822ad221ec15d
creator154 <cteator898@gmail.com>
2026-05-17 12:36:21 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -305,7 +305,7 @@ async def txt_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
 
             elif "youtube.com" in url or "youtu.be" in url:
-                cmd = f'yt-dlp --cookies youtube_cookies.txt -f "{ytf}" "{url}" -o "{name}".mp4'
+    cmd = f'yt-dlp --cookies youtube_cookies.txt --no-check-certificate -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             else:
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
