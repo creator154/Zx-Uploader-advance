6c76f3c529f0270ac683f232f1039e369dcdcb7e
creator154 <cteator898@gmail.com>
2026-05-22 11:58:19 +0530

    Update CREDIT variable to new username

--- modules/vars.py
+++ modules/vars.py
@@ -4,4 +4,4 @@ API_ID    = os.environ.get("API_ID", "11867213")
 API_HASH  = os.environ.get("API_HASH", "d475e13d8cf6937316d9fb8df5a049f9")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-CREDIT = "@Itz_Sumit"
+CREDIT = "@Oxyzen_Zx"
