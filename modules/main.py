378a827e9722250f2877c750c31d1eeadc2a8ec0
creator154 <cteator898@gmail.com>
2026-07-08 19:56:10 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -47,9 +47,9 @@ async def root_route_handler(request):
 )
 
 async def web_server():
-web_app = web.Application(client_max_size=30000000)
-web_app.add_routes(routes)
-return web_app
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
 
 async def start_bot():
 await bot.start()
