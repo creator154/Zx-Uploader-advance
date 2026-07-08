fa446aa02a01aaa8ad2061cb38357b3ef9c19dea
creator154 <cteator898@gmail.com>
2026-07-08 10:40:03 +0530

    Update credit to new username '@SumitTripathi'

--- modules/vars.py
+++ modules/vars.py
@@ -4,4 +4,4 @@ API_ID    = os.environ.get("API_ID", "37721193")
 API_HASH  = os.environ.get("API_HASH", "ed5cbbc0e14a777e1b2deb0c3f763874")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-CREDIT = "@Itz_Sumit"
+CREDIT = "@SumitTripathi"
