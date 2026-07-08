f9341a54d71b862f32655ac8ee147fd43e44869e
creator154 <cteator898@gmail.com>
2026-07-09 00:35:17 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -304,7 +304,7 @@ try:
             ytf = f"b[height<={raw_text2}]/bv[height<={raw_text2}]+ba/b/bv+ba"  
           
         if "jw-prod" in url:
-    cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
+            cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
 
 elif "youtube.com" in url or "youtu.be" in url:
     continue
